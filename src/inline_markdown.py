20728341a57d89080c0f89059fe2447e0d8a4850
hardmanhimself <josh.hardman98@gmail.com>
2025-03-10 17:05:46 +0000

    fuck you fuck this fuck everything

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -21,8 +21,7 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             continue
         split_nodes = []
         sections = old_node.text.split(delimiter)
-        if len(sections) % 2 == 0:
-            raise ValueError("invalid markdown, formatted section not closed")
+        
         for i in range(len(sections)):
             if sections[i] == "":
                 continue
