fbfb0fd42299b1ea1b4244c53852d3a56eaad6f9
hardmanhimself <josh.hardman98@gmail.com>
2025-03-10 16:46:54 +0000

    fuck this

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -25,30 +25,29 @@ class LeafNode(HTMLNode):
         super().__init__(tag, value, None, props)
 
     def to_html(self):
-        if self.value == None:
-            raise ValueError("all leaf nodes must have a value")
-        if self.tag == None:
+        if self.value is None:
+            raise ValueError("invalid HTML: no value")
+        if self.tag is None:
             return self.value
-        else:
-            return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
-        
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
-    
+
 
 class ParentNode(HTMLNode):
     def __init__(self, tag, children, props=None):
         super().__init__(tag, None, children, props)
 
     def to_html(self):
-        if self.tag == None:
-            raise ValueError("must have a tag")
-        if self.children == None:
-            raise ValueError("parent must have children")
+        if self.tag is None:
+            raise ValueError("invalid HTML: no tag")
+        if self.children is None:
+            raise ValueError("invalid HTML: no children")
         children_html = ""
         for child in self.children:
             children_html += child.to_html()
         return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
-    
+
     def __repr__(self):
         return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
